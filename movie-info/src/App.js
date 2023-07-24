f08d713068203111dbcb1482aa9758596f256a25
신관우 <tlsrhksdn23@gmail.com>
2023-07-24 12:02:02 +0000

    #드디어 #리엑트 # 구동성공 리엑트를 다루는 기술 책 공부 시작

--- movie-info/src/App.js
+++ movie-info/src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from "react";
 
 function App() {
+  const [toDo, setToDO] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const onChange = (event) => setToDo(event.target.value);
+  const onSubmit = (event) => {
+	  event.preventDefault();
+	  if (toDO === ""){
+		  return;
+	  }
+	  setToDo("");
+	  setToDos(currentArray => [toDo, ]);
+  };
+  console.log(toDos);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+	<div>
+		  <form onSubmit = {onSubmit}>
+		  	<input
+				onChange = {onChange}
+				value = {toDo}
+				type = "text"
+				placeholder = "Write your to do..."
+		  </form>
+			
+
+	</div>
   );
 }
 
